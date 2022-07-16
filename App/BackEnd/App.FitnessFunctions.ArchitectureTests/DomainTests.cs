edf1398e5c3984b8eae56ac383251ae337cccf1a
James Tayler <james@tayler.io>
2022-07-17 02:04:05 +1200

    update arch unit rules to improve testing

--- App/BackEnd/App.FitnessFunctions.ArchitectureTests/DomainTests.cs
+++ App/BackEnd/App.FitnessFunctions.ArchitectureTests/DomainTests.cs
@@ -1,4 +1,6 @@
+using System.Data.Common;
 using System.Security.Claims;
+using App.Core.Context;
 using ArchUnitNET.Domain.Extensions;
 using ArchUnitNET.Fluent.Conditions;
 using ArchUnitNET.xUnit;
@@ -103,6 +105,17 @@ namespace App.FitnessFunctions.ArchitectureTests
                 .Should()
                 .NotDependOnAny(typeof(ClaimsPrincipal))
                 .Because("that should be handled by abstractions in App.Core. This is a notoriously tricky class to instantiate correctly. See: https://www.benday.com/2021/08/13/3-common-problems-with-claimsidentity-and-claimsprincipal-in-asp-net-core/")
+                .Check(_application.Architecture);
+        }
+        
+        [Fact]
+        public void RepositoriesShouldNotDirectlyReferenceUserContext()
+        {
+            Classes().That().Are(_application.DomainClasses)
+                .And().DependOnAny(typeof(DbConnection))
+                .Should()
+                .NotDependOnAny(typeof(IUserContext))
+                .Because("repositories should be passed all the data they are expected to persist/query. This makes testing more flexible and requires less mocking when you want to test behavior that differs between authenticated and unauthenticated users.")
                 .Check(_application.Architecture);
         }
     }
