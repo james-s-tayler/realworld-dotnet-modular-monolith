4ab7d3a1c472d24ec8bf7f1ae37c87f2019f00fc
solo yolo <solo@yolo.com>
2025-07-23 13:49:15 +0900

    require all fields when updating an article

--- App/BackEnd/App.Content.Domain.Contracts/DTOs/EditArticleDTO.cs
+++ App/BackEnd/App.Content.Domain.Contracts/DTOs/EditArticleDTO.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using App.Core.DataAccess;
 
@@ -5,9 +6,12 @@ namespace App.Content.Domain.Contracts.DTOs
 {
     [ExcludeFromCodeCoverage]
     public class EditArticleDTO : ContractModel
-    {
+    { 
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Body { get; set; }
     }
 }
